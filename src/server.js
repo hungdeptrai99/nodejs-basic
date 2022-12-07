3fd6fef0aa7f3c730852b8d262dea8fbbfb5f551
Tran Gia Hung <hungtg@rikkeisoft.com>
2022-12-07 16:11:24 +0700

    form -postRequest

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
 // const express = require("express");
 import  express  from "express";
 import configViewEngine from "./configs/viewEngine";
-import initWebRoute from './route/web'
-import connection from './configs/connectDB'
+import initWebRoute from './route/web';
 require('dotenv').config();
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 //setup view engine
 configViewEngine(app);
 // init web route
