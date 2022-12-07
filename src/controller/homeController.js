e0faca345d6dfaea336c6409dc66943591a97584
Tran Gia Hung <hungtg@rikkeisoft.com>
2022-12-07 17:16:12 +0700

    edit,delte

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -18,11 +18,36 @@ let createNewUser = async (req, res) => {
     `insert into users(firstName,lastName,email,address) values(?,?,?,?)`,
     [firstName, lastName, email, address]
   );
-  return res.redirect('/')
+  return res.redirect("/");
+};
+let deleteUser = async (req, res) => {
+  let userId = req.body.userId;
+  await pool.execute(`delete from users where id=?`, [userId]);
+  return res.redirect("/");
+};
+let getEditPage = async (req, res) => {
+  let id = req.params.userId;
+  let [user] = await pool.execute(`select * from users where id = ?`, [id]);
+  // await pool.execute(`update users set firstName`)
+  return res.render("update.ejs", { dataUser: user[0] });
+};
+let updateUser = async (req, res) => {
+  let { firstName, lastName, email, address, id } = req.body;
+  await pool.execute(
+    "update users set firstName = ? , lastName=?,email=?,address=? where id = ?",
+    [firstName, lastName, email, address, id]
+  );
+  return res.redirect("/");
 };
 
+// UPDATE table_name
+// SET column1 = value1, column2 = value2, ...
+// WHERE condition;
 module.exports = {
   getHomePage,
   getDetailPage,
   createNewUser,
+  deleteUser,
+  getEditPage,
+  updateUser,
 };
